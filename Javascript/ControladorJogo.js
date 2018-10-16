03666e678210204a4cc2afee2cb7184de226d81d
LucaDillenburg <luca.assumpcao.dillenburg@gmail.com>
2018-10-16 13:07:46 -0300

    REESTRUTURAÇÃO+ Colisão certa em classe Tiro e Personagem
    Novo:
     - reestruturação do projeto (classes, forma geométrica)
     - tiro pode andar para qualquer direção inclusive oblíquo
     - tiro para no personagem, obstáculos e inimigos
     - deixar a classe ObjetoTela com formaGeometrica qualquer (pode ser triângulo, quadrado, retangulo, paralelogramo, quadrilátero).
    fazer get pra isso (entao os metodos de alterar tais formas nao precisarao estar nos ObjetosTela)
     - printar tiro morto de cor diferente
     - colisao de tiro e personagem pronta
    
    ps: não está testado

--- Javascript/ControladorJogo.js
+++ Javascript/ControladorJogo.js
@@ -1,7 +1,4 @@
-var EstadoJogo = {"NaoComecou":0, "Jogando":1, "Pausado":2, "Morto":3};
-Object.freeze(EstadoJogo);
-var Direcao = {"Direita":1, "Esquerda":2, "Cima":3, "Baixo":4};
-Object.freeze(Direcao);
+const EstadoJogo = {"NaoComecou":0, "Jogando":1, "Pausado":2, "Morto":3};
 
 const heightVidaUsuario = 30;
 class ControladorJogo
@@ -11,8 +8,11 @@ class ControladorJogo
     this._personagemPrincipal = null;
     this._inimigos = null;
     this._obstaculos = null;
+    this._controladorTiros = null;
 
     this._estadoJogo = EstadoJogo.NaoComecou;
+    this._estadoJogoAnterior = null;
+
     this._level = 1;
     this._passandoLevel = false;
   }
@@ -21,139 +21,179 @@ class ControladorJogo
   get estadoJogo()
   { return this._estadoJogo; }
 
+  static personagemPrincPadrao()
+  {
+    let tamPersPrinc = 30;
+    let corPers = color(0, 51, 153);
+    let forma = new Quadrado(0, 0, tamPer, corPers, corPers, null);
+
+    let pers = new PersonagemPrincipal(forma, 100, ControladorJogo.tiroPersPadrao, 8);
+    pers.colocarLugarInicial();
+    return
+  }
+  static tiroPersPadrao()
+  {
+    let corTiro = color(0, 0, 102);
+    let tiro = new Tiro(new Retangulo(0, 0, 5, 8, corTiro, corTiro, null),
+                    {fill: color("black"), stroke: color("black")}, 0, -15, true, 5);
+    tiro.colocarNoMeioX();
+    return tiro;
+  }
+
   //inicializacao
   comecarJogo()
   {
-    let tamPersPrinc = 30;
-                                                     //(x,                      y,           tamLado, 
-    this._personagemPrincipal = new PersonagemPrincipal((width-tamPersPrinc)/2, 0.75*height, tamPersPrinc,
-    //qtdAndar, cor,               vida, widthTiroPadrao, heightTiroPadrao, corTiroPadrao,    qtdAndarTiro)
-      8,        color(0, 51, 153), 100,  5,               8,                color(0, 0, 102), 15);
+    this._personagemPrincipal = ControladorJogo.personagemPrincPadrao();
 
     this._level = 1;
-    this.iniciarLevel();
+    this._iniciarLevel();
     this._estadoJogo = EstadoJogo.Jogando;
   }
   //inicializar level
-  iniciarLevel()
+  _iniciarLevel()
   {
     this._passandoLevel = true;
     this._inimigos = null;
     this._obstaculos = null;
-    
+
     switch(this._level)
     {
       case 1:
         this._inimigos = new Array(1);
         let tamInimigo = 50;
-        this._inimigos[0] = new Inimigo((width - tamInimigo)/2, tamInimigo + 10, tamInimigo, tamInimigo, color("red"), 250, color("red"), 5, 5, color("red"), 10);
+        let corInim = color("red");
+
+        let formaInimigo = new Quadrado(0, 0, tamInimigo, corInim, corInim, null);
+
+        this._inimigos[0] = new Inimigo(formaInimigo, 250, corInim, null, 8, false);
         break;
     }
-    
-    setTimeout(this._auxPassandoLevel, 3000);
+
+    setTimeout(function() {this._passandoLevel = false;}, 3000);
   }
-  _auxPassandoLevel() { this._passandoLevel = false; }
   //passar de level
+  _passarLevel()
+  {
+    this._level++;
+    this._iniciarLevel();
+  }
 
 
   //JOGO
   //andar tiros
-  andarTiros()
+  _andarTiros()
   {
     //desenha o personagem e os tiros dele
-    this._personagemPrincipal.andarTiros();
+    this._personagemPrincipal.andarTiros(this._personagemPrincipal, this._obstaculos, this._inimigos);
 
     if (this._inimigos != null)
       for (let i = 0; i<this._inimigos.length; i++)
-        this._inimigos[i].andarTiros();
+        this._inimigos[i].andarTiros(this._personagemPrincipal, this._obstaculos, this._inimigos);
   }
-  
+
 
   //funcionalidades personagem
-  andarPers(direcao)
+  andarPers(direcaoX, direcaoY) //setinhas
   {
-    switch (direcao)
-    {
-      case Direcao.Cima:
-        this._personagemPrincipal.andarY(false);
-        break;
-      case Direcao.Baixo:
-        this._personagemPrincipal.andarY(true);
-        break;
-      case Direcao.Direita:
-        this._personagemPrincipal.andarX(true);
-        break;
-      case Direcao.Esquerda:
-        this._personagemPrincipal.andarX(false);
-        break;
-    }
+    //se estah jogando (jah comecou, nao estah pausado e o personagem nao morreu)
+    if (this._estadoJogo == EstadoJogo.Jogando)
+      this._personagemPrincipal.andar(direcaoX, direcaoY, this._obstaculos, this._inimigos, this._controladorTiros);
   }
-  atirar()
+  atirar() //espaco
   {
     this._personagemPrincipal.adicionarTiro();
+  }
+  mudarPausado() //enter
+  {
+    if (this._estadoJogo == EstadoJogo.Pausado)
+    {
+      this._estadoJogo = this._estadoJogoAnterior;
+      this._estadoJogoAnterior = null;
+    }else
+    {
+      this._estadoJogoAnterior = this._estadoJogo;l
+      this._estadoJogo = EstadoJogo.Pausado;
+    }
   }
 
 
   //draw
   draw()
   {
-    switch (this._estadoJogo)
+    if (this._estadoJogo == EstadoJogo.NaoComecou)
     {
-      case EstadoJogo.NaoComecou:
-        background(255);
-        fill(0);
-        text("Pressione [ESC] para começar a jogar", 50, 50);
-        break;
-      case EstadoJogo.Jogando:
-        background(100);
+      background(255);
+      stroke(0);
+      fill(0);
+      textSize(40);
+      text("Pressione [ESC] para começar a jogar", 50, 50);
+      return;
+    }
+    if (this._estadoJogo == EstadoJogo.Pausado) //pausa-se com [ENTER]
+    {
+      //deixa o que estava na tela de fundo mesmo
+      stroke(0);
+      fill(0);
+      textSize(40);
+      text("PAUSADO: Pressione [ENTER] para despausar", 50, 50);
+      return;
+    }
+    if (this._estadoJogo == EstadoJogo.Morto) //animacao dele morrendo
+    {
 
-        //desenha o personagem e os tiros dele
-        this._personagemPrincipal.draw();
+      return;
+    }
 
-        if (this._inimigos != null)
-          for (let i = 0; i<this._inimigos.length; i++)
-            this._inimigos[i].draw();
+    //daqui pra baixo this._estadoJogo == EstadoJogo.Jogando
 
-        if (this._obstaculos != null)
-          for (let i = 0; i<this._obstaculos.length; i++)
-            this._obstaculos[i].draw();
+    background(100);
 
-        this.andarTiros();
-        
-        if (this._passandoLevel)
-        {
-          textSize(40);
-          fill(0);
-          textAlign(CENTER, CENTER);
-          text("Level " + this._level, width/2, height/2);
-          textAlign(LEFT, BASELINE);
-        }
+    //desenha o personagem e os tiros dele
+    this._personagemPrincipal.draw();
 
-        this.colocacarVidaUsuarioTela();
+    if (this._inimigos != null)
+      for (let i = 0; i<this._inimigos.length; i++)
+        this._inimigos[i].draw();
 
-        break;
-      case EstadoJogo.Pausado:
-        //pausa-se com [ENTER]
-        break;
-      case EstadoJogo.Morto:
-        //animacao dele morrendo
-        break;
+    if (this._obstaculos != null)
+      for (let i = 0; i<this._obstaculos.length; i++)
+        this._obstaculos[i].draw();
+
+    this._andarTiros();
+
+    if (this._passandoLevel)
+    {
+      textSize(40);
+      fill(0);
+      textAlign(CENTER, CENTER);
+      text("Level " + this._level, width/2, height/2);
+      textAlign(LEFT, BASELINE);
+    }
+
+    this._personagemPrincipal.colocacarVidaTela();
+
+    if (this._acabouLevel())
+      this._passarLevel();
+    else
+    {
+      //fazer alguma coisa dependendo do level
+      switch (this._level)
+      {
+        case 1:
+          break;
+      }
     }
   }
-    
-  colocacarVidaUsuarioTela()
-  {
-    stroke(0);
-    fill(color("black"));
-    rect(0, height - heightVidaUsuario, width, heightVidaUsuario);
 
-    noStroke(0);
-    fill(color("green"));
-    rect(0, height - heightVidaUsuario, width*(this._personagemPrincipal._vida/this._personagemPrincipal._vidaMAX), heightVidaUsuario);
-    
-    fill(0);
-    let fontSize = 22;
-    textSize(fontSize);
-    text("Vida: " + this._personagemPrincipal._vida + "/" + this._personagemPrincipal._vidaMAX, 5, height - heightVidaUsuario + fontSize);
+  _acabouLevel()
+  {
+    //verificar se o level jah acabou
+    switch (this._level)
+    {
+      case 1:
+        return this._inimigos[0].vida <= 0;
+      default:
+        return false;
+    }
   }
 }
--- Javascript/ObjetosComplexos.js
+++ Javascript/ObjetosComplexos.js
@@ -1,68 +1,269 @@
+class PersComTiros extends Objeto
+{
+  constructor(formaGeometrica, vida, tiroPadrao, ehPersPrinc)
+  {
+    //personagem
+    super(formaGeometrica);
+    this._vida = vida;
+    this._vidaMAX = vida;
+    this._ehPersPrincipal = ehPersPrinc;
+
+    this._controladorTiros = new ControladorTiros(tiroPadrao, ehPersPrinc);
+  }
+
+  //get ControladorTiros
+  get controladorTiros()
+  { return this._controladorTiros; }
+
+  //getter vida
+  get vida()
+  { return this._vida; }
+  get vidaMAX()
+  { return this._vidaMAX; }
+
+  //mudar vida
+  colocarMAXVida()
+  { this._vida = this._vidaMAX; }
+  zerarVida()
+  { this._vida = 0; }
+  mudarVida(qtdMuda)
+  {
+    this._vida += qtdMuda;
+    if (this._vida < 0)
+        this._vida = 0;
+    return this._vida != 0;
+  }
+
+  //getters and setters tiro
+  get tiroPadrao()
+  { return this._tiroPadrao; }
+  set tiroPadrao(tiro)
+  { this._tiroPadrao = tiro; }
+
+  //TIROS
+  //novo tiro
+  adicionarTiro(ondeColocar, qtdAndarX, qtdAndarY, corMorto, mortalidade, formaGeomTiro)
+  //essa eh a ordem onde os primeiros parametros da funcao sao os que primeiro estariam fora do padrao
+	//pode-se chamar uma funcao sem todos os parametros necessarios e os demais ficam como nulos,
+		//porem se for colocar parametros tem que estar na ordem certa
+  {
+    if (ondeColocar == null)
+      ondeColocar = direcaoPadrao();
+    let onde = lugarCertoTiro(ondeColocar);
+    this._controladorTiros.adicionarTiro(onde.x, onde.y, ondeColocar, qtdAndarX, qtdAndarY, corMorto, mortalidade, formaGeomTiro);
+  }
+  adicionarTiroDif(tiro, ondeColocar)
+  {
+    if (ondeColocar == null)
+      ondeColocar = direcaoPadrao();
+    let onde = lugarCertoTiro(ondeColocar);
+    //criar tiro e adicionar ao comeco da lista
+		this._controladorTiros.adicionarTiroDif(onde.x, onde.y, tiro);
+  }
+  direcaoPadrao()
+  {
+    if (this._ehPersPrincipal)
+      return Direcao.Cima;
+    else
+      return Direcao.Baixo;
+  }
+  lugarCertoTiro(ondeColocar)
+  {
+    let x = null;
+    let y = null;
+    switch (ondeColocar)
+    {
+      case Direcao.Cima:
+        x = this._formaGeometrica.x + (this._formaGeometrica.width - tiro.formaGeometrica.width)/2;
+        y = this._formaGeometrica.y - tiro.formaGeometrica.height;
+        break;
+      case Direcao.Baixo:
+        x = this._formaGeometrica.x + (this._formaGeometrica.width - formaGeometrica.width)/2;
+        y = this._formaGeometrica.y + this._formaGeometrica.height;
+        break;
+      case Direcao.Esquerda:
+        x = this._formaGeometrica.x - tiro.formaGeometrica.width;
+        y = this._formaGeometrica.y + (this._formaGeometrica.height - tiro.formaGeometrica.height)/2;
+        break;
+      case Direcao.Direita:
+        x = this._formaGeometrica.x + this._formaGeometrica.width;
+        y = this._formaGeometrica.y + (this._formaGeometrica.height - tiro.formaGeometrica.height)/2;
+        break;
+    }
+
+    return {x: x, y: y};
+  }
+
+  //mover tiros
+  andarTiros(pers, obstaculos, inimigos)
+  {
+    this._controladorTiros.andarTiros();
+  }
+
+	//draw
+    //desenha o personagem e todos seus tiros
+	draw()
+	{
+		super.draw();
+    this._controladorTiros.draw(); //desenha tiros
+	}
+
+
+  //MUDAR TAMANHOS
+  set width(nvWidth)
+  {
+    if (this._formaGeometrica.codForma != Geometria.COD_QUADRADO
+      && this._formaGeometrica.codForma != Geometria.COD_RETANGULO)
+      throw "Não é possível mudar o width de um objeto que não tem!";
+
+    if (nvWidth < 0)
+      throw "Width inválido!";
+
+    if (this._formaGeometrica.codForma == Geometria.COD_QUADRADO)
+      this._formaGeometrica.tamanhoLado = nvWidth;
+    else
+      this._formaGeometrica.width = nvWidth;
+  }
+  set height(nvHeight)
+  {
+    if (this._formaGeometrica.codForma != Geometria.COD_QUADRADO
+      && this._formaGeometrica.codForma != Geometria.COD_RETANGULO)
+      throw "Não é possível mudar o width de um objeto que não tem!";
+
+    if (nvHeight < 0)
+      throw "Height inválido!";
+
+    if (this._formaGeometrica.codForma == Geometria.COD_QUADRADO)
+      this._formaGeometrica.tamanhoLado = nvHeight;
+    else
+      this._formaGeometrica.height = nvHeight;
+  }
+  mudarTamLados(qtdMudar)
+  {
+    if (this._formaGeometrica.codForma != Geometria.COD_QUADRADO
+      && this._formaGeometrica.codForma != Geometria.COD_RETANGULO)
+      throw "Não é possível mudar o width e height de um objeto que não tem!";
+
+    if (this._formaGeometrica.width + qtdMudar < 0 || this._formaGeometrica.height + qtdMudar < 0)
+      throw "QtdMuda tamanho lados inválido!";
+
+    if (this._formaGeometrica.codForma == Geometria.COD_QUADRADO)
+      this._formaGeometrica.mudaTamanhoLado(qtdMudar);
+    else
+    {
+      this._formaGeometrica.mudarWidth(qtdMudar);
+      this._formaGeometrica.mudarHeight(qtdMudar);
+    }
+  }
+}
+
+
 class PersonagemPrincipal extends PersComTiros
 {
-  constructor(x, y, tamLado, qtdAndar, cor, vida, widthTiroPadrao, heightTiroPadrao, corTiroPadrao, qtdAndarTiro)
+  constructor(formaGeometrica, vida, tiroPadrao, qtdAndar)
   {
-    super(x, y, tamLado, tamLado, cor, vida, widthTiroPadrao, heightTiroPadrao, corTiroPadrao, qtdAndarTiro);
+    super(formaGeometrica, vida, tiroPadrao, true);
     this._qtdAndar = qtdAndar;
   }
 
   //mudar (x,y)
-  set x(x)
+  colocarLugarInicial()
   {
-    if (x >= 0 && x + this._width <= width)
-      this._x = x;
+    this.colocarMeioX();
+    this._formaGeometrica.y = 0.75*height;
   }
+  andar(direcaoX, direcaoY, obstaculos, inimigos, controladorTirosJogo)
+  //usuario soh usa esse metodo
+  {
+    let qtdAndarX;
+    switch(direcaoX)
+    {
+      case Direcao.Direita:
+        qtdAndarX = this._qtdAndar;
+        break;
+      case Direcao.Esquerda:
+        qtdAndarX = -this._qtdAndar;
+        break;
+      default:
+        qtdAndarX = 0;
+        break;
+    }
+
+    let qtdAndarY;
+    switch(direcaoY)
+    {
+      case Direcao.Baixo:
+        qtdAndarY = this._qtdAndar;
+        break;
+      case Direcao.Cima:
+        qtdAndarY = -this._qtdAndar;
+        break;
+      default:
+        qtdAndarX = 0;
+        break;
+    }
+
+    this.mudarXY(qtdAndarX, qtdAndarY, obstaculos, inimigos, controladorTirosJogo);
+  }
+  mudarXY(qtdMudaX, qtdAndarY, obstaculos, inimigos, controladorTirosJogo)
+  //retorna se pode andar tudo aquilo ou nao (para andar do obstaculo)
+  {
+    let qtdVaiMudarX = Tela.qtdAndarObjNaoSairX(this._formaGeometrica, qtdMudaX);
+    let qtdVaiMudarY = Tela.qtdAndarObjNaoSairY(this._formaGeometrica, qtdMudaY);
+
+    //obstaculos
+    if (obstaculos != null)
+    {
+      let menorHipotenusa = Operacoes.hipotenusa(qtdPodeAndarX, qtdPodeAndarY);
+      for(let i = 0; i<obstaculos.length; i++)
+      {
+        let qtdPodeAndar = Interseccao.qntPodeAndarAntesIntersec(obstaculos[i].formaGeometrica, this._formaGeometrica, qtdPodeAndarX, qtdPodeAndarY);
+        let hipotenusa = Operacoes.hipotenusa(qtdPodeAndar.x, qtdPodeAndar.y);
+
+        //se tiro vai bater em um obstaculo mais perto que o outro
+        if (hipotenusa < menorHipotenusa)
+        {
+          menorHipotenusa = hipotenusa;
+          qtdVaiMudarX = qtdPodeAndar.x;
+          qtdVaiMudarY = qtdPodeAndar.y;
+        }
+      }
+    }//aqui tudo o que devia ser feito com obstaculos estah OK
+
+    //controladoresTiros
+    let qtdInimigos = (inimigos==null)?0:inimigos.length;
+    for (let i = 0; i<qtdInimigos+1; i++)
+    {
+      let controladorTirosAtual;
+      if (i<qtdInimigos)
+        controladorTirosAtual = inimigos[i].controladorTiros;
+      else
+        controladorTirosAtual = controladorTirosJogo;
+      //controladorTirosAtual passará por todos os controladores de tiros dos inimigos e do jogo em si
+
+      controladorTirosAtual.procedimentoObjTelaColideAndar(this, qtdVaiMudarX, qtdVaiMudarY);
+    }
+
+    //inimigos
+    for (let i = 0; i<qtdInimigos; i++)
+      if (Interseccao.vaiTerInterseccao(inimigos[i].formaGeometrica, this._formaGeometrica, qtdVaiMudarX, qtdVaiMudarY))
+      {
+        inimigos[i].tirarVidaPersIntersec(this);
+        //TODO: se personagem ficar dentro do inimigo ir tirando vida
+      }
+
+    //aqui qtdVaiMudarX e qtdVaiMudarY sao os maiores possiveis (a menor distancia que bateu)
+    this._formaGeometrica.x += qtdVaiMudarX;
+    this._formaGeometrica.y += qtdVaiMudarY;
+
+    //se consegue andar tudo o que deveria
+    return qtdVaiMudarX == qtdMudaX && qtdVaiMudarY == qtdMudaY;
+  }
+
   get x()
-  { return this._x; }
-  mudarX(qtdMuda)
-  {
-    let retSair = this.vaiSairTelaX(qtdMuda);
-
-    if (retSair == PersonagemPrincipal.SAIU_ESQUERDA
-        || (retSair == PersonagemPrincipal.SAIU_DIREITA_E_ESQUERDA && qtdMuda < 0))
-      this._x = 0;
-    else
-    if (retSair == PersonagemPrincipal.SAIU_DIREITA
-        || (retSair == PersonagemPrincipal.SAIU_DIREITA_E_ESQUERDA && qtdMuda > 0))
-      this._x = width - this._width;
-    else
-      this._x += qtdMuda;
-  }
-  andarX(direita)
-  {
-    if (direita)
-      this.mudarX(this._qtdAndar);
-    else
-      this.mudarX(-this._qtdAndar);
-  }
-  set y(y)
-  {
-    if (y >= 0 && y + this._height <= height - heightVidaUsuario)
-      this._y = y;
-  }
+  { return this._formaGeometrica.x; }
   get y()
-  { return this._y; }
-  mudarY(qtdMuda)
-  {
-    let retSair = this.vaiSairTelaY(qtdMuda);
-
-    if (retSair == PersonagemPrincipal.SAIU_EM_CIMA
-      	|| (retSair == PersonagemPrincipal.SAIU_EM_CIMA_E_EMBAIXO && qtdMuda < 0))
-      this._y = 0;
-    else
-    if (retSair == PersonagemPrincipal.SAIU_EMBAIXO
-        || (retSair == PersonagemPrincipal.SAIU_EM_CIMA_E_EMBAIXO && qtdMuda > 0))
-      this._y = height - this._height - heightVidaUsuario;
-    else
-      this._y += qtdMuda;
-  }
-  andarY(baixo)
-  {
-    if (baixo)
-      this.mudarY(this._qtdAndar);
-    else
-      this.mudarY(-this._qtdAndar);
-  }
+  { return this._formaGeometrica.y; }
 
   //mudar qtdAndar
   get qtdAndar()
@@ -70,114 +271,98 @@ class PersonagemPrincipal extends PersComTiros
   set qtdAndar(qtdAndar)
   { this._qtdAndar = qtdAndar; }
 
-  //mudarTamLado
-  set tamLado(nvTamLado)
+  //draw
+  colocacarVidaTela()
   {
-    if (nvTamLado < 0)
-      throw "Tamanho de lado inválido!";
-    this._width = nvTamLado;
-    this._height = nvTamLado;
-  }
-  mudarTamLado(qtdMuda)
-  {
-    if (this._width + qtdMuda < 0)
-      throw "QtdMuda tamanho de lado inválido!";
-    this._width += qtdMuda;
-    this._height += qtdMuda;
+    stroke(0);
+    fill(255);
+    rect(0, height - heightVidaUsuario, width, heightVidaUsuario);
+
+    noStroke(0);
+    fill(color("green"));
+    rect(tamStroke, height - heightVidaUsuario + tamStroke,
+      (width - 2*tamStroke)*(this._personagemPrincipal._vida/this._personagemPrincipal._vidaMAX), heightVidaUsuario - 2*tamStroke);
+
+    fill(0);
+    let fontSize = 22;
+    textSize(fontSize);
+    text("Vida: " + this._personagemPrincipal._vida + "/" + this._personagemPrincipal._vidaMAX, 5, height - heightVidaUsuario + fontSize);
   }
 }
 
-class Tiro extends ObjetoTela
-{
-  constructor(x, y, height, width, cor, qtdAndar, qntTiraVida)
-  {
-    super(x, y, height, width, cor);
-    this._qtdAndar = qtdAndar;
-    this._qntTiraVida = qntTiraVida;
-  }
-
-  andar()
-  {
-    this._y -= this._qtdAndar;
-    return this._y + this._height > 0;
-  }
-
-  tirarVidaPers(pers)
-  {
-    pers.mudarVida(-this._qntTiraVida);
-  }
-}
 
 class Inimigo extends PersComTiros
 {
-  constructor(x, y, width, height, cor, vida, corVida, widthTiroPadrao, heightTiroPadrao, corTiroPadrao, qtdAndarTiro)
+  constructor(formaGeometrica, vida, corVida, tiroPadrao, qtdTiraVidaPersQndIntersec, objSaiuTotalmente)
   {
-    super(x, y, width, height, cor, vida, widthTiroPadrao, heightTiroPadrao, corTiroPadrao, qtdAndarTiro);
+    super(formaGeometrica, vida, tiroPadrao, false);
+
     this._corVida = corVida;
 
     this._widthVida = this._vidaMAX*0.25;
     this._heightVida = 10;
-  }
-  
-  set height(height)
-  { this._height = height; }
-  mudarHeight(qtdMuda)
-  {
-    this._height += qtdMuda;
-    if (this._height < 0)
-      this._height = 0;
-    return this._height > 0;
+
+    this._objSaiuTotalmente = objSaiuTotalmente;
+
+    this._qtdTiraVidaPersQndIntersec = qtdTiraVidaPersQndIntersec;
   }
 
-  set width(width)
-  { this._width = width; }
-  mudarwidth(qtdMuda)
+  //(x, y)
+  mudarX(qtdMudar)
   {
-    this._width += qtdMuda;
-    if (this._width < 0)
-      this._width = 0;
-    return this._width > 0;
+    //verificar se nao bate em personagem
+    this._formaGeometrica.x += qtdMudar;
+    if (this._objSaiuTotalmente)
+    //se querem saber se inimigo saiu totalmente
+      return !Tela.objSaiuTotalmente(this._formaGeometrica);
+    else
+    //se querem saber se inimigo saiu pelo menos um pouco
+      return !Tela.objSaiuEmX(this._formaGeometrica);
   }
-  
+  mudarY(qtdMudar)
+  {
+    //verificar se nao bate em personagem
+    this._formaGeometrica.y += qtdMudar;
+    if (this._objSaiuTotalmente)
+    //se querem saber se inimigo saiu totalmente
+      return !Tela.objSaiuTotalmente(this._formaGeometrica);
+    else
+    //se querem saber se inimigo saiu pelo menos um pouco
+      return !Tela.objSaiuEmY(this._formaGeometrica);
+  }
+
+  //tirar vida personagem quando intersecta com inimigo
+  get qtdTiraVidaPersQndIntersec()
+  { return this._qtdTiraVidaPersQndIntersec; }
+  tirarVidaPersIntersec(pers)
+  { pers.mudarVida(-this._qtdTiraVidaPersQndIntersec); }
+
+  //desenho
   draw()
   {
+    //desenha inimigo e tiros dele
     super.draw();
-    
+
+    //desenha vida:
+
+    let tamStrokeAtual = 0.8;
+    strokeWeight(tamStrokeAtual);
+
+    //DRAW VIDA:
     //draw vida em cima do inimigo
-    let xVida = this._x + (this._width - this._widthVida)/2;
-    let yVida = this._y - (this._heightVida + 5);
-    
+    let xVida = this._formaGeometrica.x + (this._formaGeometrica.width - this._widthVida)/2;
+    let yVida = this._formaGeometrica.y - (this._heightVida + 5);
+
     //desenhar parte branca da vida
     stroke(0);
     fill(255);
     rect(xVida, yVida, this._widthVida, this._heightVida);
-    
     //desenhar a parte verdadeira
     noStroke();
     fill(this._corVida);
-    rect(xVida, yVida, this._widthVida*(this._vida/this._vidaMAX), this._heightVida);
-  }
-}
+    rect(xVida + tamStrokeAtual, yVida + tamStrokeAtual,
+      (this._widthVida - 2*tamStrokeAtual)*(this._vida/this._vidaMAX), this._heightVida - 2*tamStrokeAtual);
 
-class Obstaculo extends ObjetoTela
-{
-  set height(height)
-  { this._height = height; }
-  mudarHeight(qtdMuda)
-  {
-    this._height += qtdMuda;
-    if (this._height < 0)
-      this._height = 0;
-    return this._height > 0;
-  }
-
-  set width(width)
-  { this._width = width; }
-  mudarwidth(qtdMuda)
-  {
-    this._width += qtdMuda;
-    if (this._width < 0)
-      this._width = 0;
-    return this._width > 0;
+    strokeWeight(tamStroke);
   }
 }
